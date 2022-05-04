76d6fb4a5ff889ffc9737112324fc888c8f5ae33
Krish Sarawgi <krishpsarawgi@gmail.com>
2022-05-04 00:33:48 -0400

    Update data_test.py

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,5 +1,11 @@
+import os
+import pytest
+
 from app.data_storage import load_auth, read_sheet, write_sheet
 
+CI_ENV = os.getenv("CI") == "true"
+
+@pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server")
 def test_data_storage():
     assert write_sheet("test", "test@test.com", "us", "general") == {
                                                                     "Name": "test",
